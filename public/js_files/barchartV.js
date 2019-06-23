121f487b75682e8cb20754686463958917af9b27
LehannG2 <37569304+LehannG2@users.noreply.github.com>
2019-06-23 15:02:23 +0200

    finished first graph

--- public/js_files/barchartV.js
+++ public/js_files/barchartV.js
@@ -1,80 +1,273 @@
 var DataArray =
 [
-	['2015', 'March', 'import', 0],
-	['2015', 'March', 'import', 0],
-	['2015', 'March', 'import', 58200368],
-	['2015', 'April', 'import', 28549589],
-	['2015', 'May', 'import', 48814896],
-	['2015', 'June', 'import', 26580294],
-	['2015', 'July', 'import', 24293499],
-	['2015', 'August', 'import', 35063958],
-	['2015', 'September', 'import', 36083344],
-	['2015', 'October', 'import', 29962966],
-	['2015', 'November', 'import', 27010608],
-	['2015', 'December', 'import', 31576496],
-	['2016', 'March', 'import', 223232323],
-	['2016', 'March', 'import', 23232323],
-	['2016', 'March', 'import', 58200368],
-	['2016', 'April', 'import', 28549589],
-	['2016', 'May', 'import', 48814896],
-	['2016', 'June', 'import', 26580294],
-	['2016', 'July', 'import', 24293499],
-	['2016', 'August', 'import', 35063958],
-	['2016', 'September', 'import', 36083344],
-	['2016', 'October', 'import', 29962966],
-	['2016', 'November', 'import', 27010608],
-	['2016', 'December', 'import', 31576496],
-	['2016', 'March', 'export', 223232323],
-	['2016', 'March', 'export', 23232323],
-	['2016', 'March', 'export', 58200368],
-	['2016', 'April', 'export', 28549589],
-	['2016', 'May', 'export', 48814896],
-	['2016', 'June', 'export', 26580294],
-	['2016', 'July', 'export', 24293499],
-	['2016', 'August', 'export', 35063958],
-	['2016', 'September', 'export', 36083344],
-	['2016', 'October', 'export', 29962966],
-	['2016', 'November', 'export', 27010608],
-	['2016', 'December', 'export', 31576496],	
-]
-var data_2015 = [];
+	['2015', 'January', 'export', 0],
+['2015', 'February', 'export', 0],
+['2015', 'March', 'export', 1639102],
+['2015', 'April', 'export', 852755],
+['2015', 'May', 'export', 1357657],
+['2015', 'June', 'export', 3237559],
+['2015', 'July', 'export', 1485326],
+['2015', 'August', 'export', 1458511],
+['2015', 'September', 'export', 998614],
+['2015', 'October', 'export', 1665762],
+['2015', 'November', 'export', 1749556],
+['2015', 'December', 'export', 1093863],
 
-for(var i=0;i<DataArray.length;i++){
-	if(DataArray[i][0]=='2015'){
-		data_2015[i] = DataArray[i][3];
-	}
-};
+['2015', 'January', 'import', 0],
+['2015', 'February', 'import', 0],
+['2015', 'March', 'import', 58200368],
+['2015', 'April', 'import', 28549589],
+['2015', 'May', 'import', 48814896],
+['2015', 'June', 'import', 26580294],
+['2015', 'July', 'import', 24293499],
+['2015', 'August', 'import', 35063958],
+['2015', 'September', 'import', 36083344],
+['2015', 'October', 'import', 29962966],
+['2015', 'November', 'import', 27010608],
+['2015', 'December', 'import', 31576496],
+
+['2016', 'January', 'export', 2302497],
+['2016', 'February', 'export', 2045941],
+['2016', 'March', 'export', 2108718],
+['2016', 'April', 'export', 2807073],
+['2016', 'May', 'export', 1086926],
+['2016', 'June', 'export', 1079102],
+['2016', 'July', 'export', 1487614],
+['2016', 'August', 'export', 1010952],
+['2016', 'September', 'export', 1879143],
+['2016', 'October', 'export', 828895],
+['2016', 'November', 'export', 599505],
+['2016', 'December', 'export', 418303],
+
+['2016', 'January', 'import', 42733029],
+['2016', 'February', 'import', 22259033],
+['2016', 'March', 'import', 37793999],
+['2016', 'April', 'import', 38885921],
+['2016', 'May', 'import', 45373248],
+['2016', 'June', 'import', 29419539],
+['2016', 'July', 'import', 47078041],
+['2016', 'August', 'import', 35449610],
+['2016', 'September', 'import', 27086061],
+['2016', 'October', 'import', 30574908],
+['2016', 'November', 'import', 27884108],
+['2016', 'December', 'import', 15264184],
+
+['2017', 'January', 'export', 1156069],
+['2017', 'February', 'export', 1077418],
+['2017', 'March', 'export', 378492],
+['2017', 'April', 'export', 2442107],
+['2017', 'May', 'export', 688706],
+['2017', 'June', 'export', 764806],
+['2017', 'July', 'export', 1301731],
+['2017', 'August', 'export', 2217086],
+['2017', 'September', 'export', 1220552],
+['2017', 'October', 'export', 1322357],
+['2017', 'November', 'export', 395755],
+['2017', 'December', 'export', 1489048],
+
+['2017', 'January', 'import', 45100403],
+['2017', 'February', 'import', 30884364],
+['2017', 'March', 'import', 30168047],
+['2017', 'April', 'import', 17800490],
+['2017', 'May', 'import', 38075177],
+['2017', 'June', 'import', 24449852],
+['2017', 'July', 'import', 45484655],
+['2017', 'August', 'import', 58474025],
+['2017', 'September', 'import', 46549540],
+['2017', 'October', 'import', 33101433],
+['2017', 'November', 'import', 22894491],
+['2017', 'December', 'import', 20514520],
+
+['2018', 'January', 'export', 719129],
+['2018', 'February', 'export', 1518987],
+['2018', 'March', 'export', 879201],
+['2018', 'April', 'export', 374929],
+['2018', 'May', 'export', 1067211],
+['2018', 'June', 'export', 326486],
+['2018', 'July', 'export', 1958271],
+['2018', 'August', 'export', 415063],
+['2018', 'September', 'export', 898085],
+['2018', 'October', 'export', 549737],
+['2018', 'November', 'export', 1572893],
+['2018', 'December', 'export', 966774],
+
+['2018', 'January', 'import', 36633780],
+['2018', 'February', 'import', 43383444],
+['2018', 'March', 'import', 38897786],
+['2018', 'April', 'import', 32467448],
+['2018', 'May', 'import', 34739771],
+['2018', 'June', 'import', 17332082],
+['2018', 'July', 'import', 30542053],
+['2018', 'August', 'import', 46012317],
+['2018', 'September', 'import', 45687471],
+['2018', 'October', 'import', 28111162],
+['2018', 'November', 'import', 41530748],
+['2018', 'December', 'import', 38632391],
+
+['2019', 'January', 'export', 2128841],
+['2019', 'February', 'export', 851873],
+['2019', 'March', 'export', 317022],
+['2019', 'April', 'export', 1008199],
+['2019', 'May', 'export', 0],
+['2019', 'June', 'export', 0],
+['2019', 'July', 'export', 0],
+['2019', 'August', 'export', 0],
+['2019', 'September', 'export', 0],
+['2019', 'October', 'export', 0],
+['2019', 'November', 'export', 0],
+['2019', 'December', 'export', 0],
+
+['2019', 'January', 'import', 51424853],
+['2019', 'February', 'import', 26340383],
+['2019', 'March', 'import', 43743970],
+['2019', 'April', 'import', 52681865],
+['2019', 'May', 'import', 0],
+['2019', 'June', 'import', 0],
+['2019', 'July', 'import', 0],
+['2019', 'August', 'import', 0],
+['2019', 'September', 'import', 0],
+['2019', 'October', 'import', 0],
+['2019', 'November', 'import', 0],
+['2019', 'December', 'import', 0]	
+];
+
+// function myFunction() {
+// 	var data1 = [];
+// 	var x = document.getElementById("mySelect").value;
+// 	var y = document.getElementById("mySelect1").value;
+// 	var t =0;
+
+// 	for(var i=0;i<DataArray.length;i++){
+// 		if(DataArray[i][0]==x && DataArray[i][2]==y){
+// 			data1[t] = DataArray[i][3];
+// 			t++;
+// 		}
+// 	};
+// 	if(y == 'import'){
+// 		barChartData.datasets[0].backgroundColor = color(window.chartColors.red).alpha(0.5).rgbString();
+// 		barChartData.datasets[0].borderColor = window.chartColors.red;
+// 		barChartData.datasets[0].data = data1;
+// 	}else{
+// 		barChartData.datasets[0].backgroundColor = color(window.chartColors.blue).alpha(0.5).rgbString();
+// 		barChartData.datasets[0].borderColor = window.chartColors.blue;
+// 		barChartData.datasets[0].data = data1;
+// 	}
+// 	window.myBar.update();
+// 	// document.getElementById("demo").innerHTML = "You selected: " + x + y;
+// }
+
 var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		var color = Chart.helpers.color;
 		var barChartData = {
 			labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
 			datasets: [{
-				label: 'Imports',
-				backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+				label: '2015',
+				backgroundColor: color(window.chartColors.orange).alpha(0.5).rgbString(),
 				borderColor: window.chartColors.red,
 				borderWidth: 1,
-				data: data_2015
+				data: []
 			}, {
-				label: 'Exports',
-				backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
+				label: '2016',
+				backgroundColor: color(window.chartColors.grey).alpha(0.5).rgbString(),
 				borderColor: window.chartColors.blue,
 				borderWidth: 1,
-				data: [
-					10,
-					20,
-					randomScalingFactor(),
-					randomScalingFactor(),
-					randomScalingFactor(),
-					randomScalingFactor(),
-					randomScalingFactor()
-				]
-			}]
+				data: []
+			},
+			{
+				label: '2017',
+				backgroundColor: color(window.chartColors.yellow).alpha(0.5).rgbString(),
+				borderColor: window.chartColors.blue,
+				borderWidth: 1,
+				data: []
+			},
+			{
+				label: '2018',
+				backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
+				borderColor: window.chartColors.blue,
+				borderWidth: 1,
+				data: []
+			},
+			{
+				label: '2019',
+				backgroundColor: color(window.chartColors.purple).alpha(0.5).rgbString(),
+				borderColor: window.chartColors.blue,
+				borderWidth: 1,
+				data: []
+			},
+		]
 
 		};
 
+		var states = [
+			['2015',true],
+			['2016',true],
+			['2017',true],
+			['2018',true],
+			['2019',true],
+		];
+		var types = [
+			['import',true],
+			['export',false]
+		]
+var years = ['2015','2016','2017','2018','2019'];
+for(var i=0;i<years.length;i++){
+	var dataImport = [];
+	var t=0;
+	for(var k=0;k<DataArray.length;k++){
+		if(DataArray[k][0]==years[i] && DataArray[k][2]=='import'){
+			dataImport[t] = DataArray[k][3];
+			t++;
+		}
+	}
+	barChartData.datasets[i].data = dataImport;
+};
+document.getElementById('export').classList.add('active');
+
+function hideData(par){
+	barChartData.datasets.forEach(function(ds){
+		if(ds.label == par){
+			for(var i=0;i<states.length;i++){
+				if(states[i][0]==par){
+					if(states[i][1]){
+						ds.hidden = true;
+						states[i][1] = false;
+						document.getElementById(par).classList.add('active');
+					}else{
+						ds.hidden = false;
+						states[i][1] = true;
+						document.getElementById(par).classList.remove('active');
+					}
+				}
+			}
+		}
+	})
+	
+	window.myBar.update();
+}
+function toggleType(type){
+	for(var i=0;i<years.length;i++){
+		var dataImport = [];
+		var t=0;
+		for(var k=0;k<DataArray.length;k++){
+			if(DataArray[k][0]==years[i] && DataArray[k][2]==type){
+				dataImport[t] = DataArray[k][3];
+				t++;
+			}
+		}
+		barChartData.datasets[i].data = dataImport;
+	};
+	if(type=='import'){
+		document.getElementById(type).classList.remove('active');
+		document.getElementById('export').classList.add('active');
+	}else{
+		document.getElementById(type).classList.remove('active');
+		document.getElementById('import').classList.add('active');
+	}
+	window.myBar.update();
+}
 		
 
-		document.getElementById('randomizeData').addEventListener('click', function() {
+		// document.getElementById('randomizeData').addEventListener('click', function() {
 			// var zero = Math.random() < 0.2 ? true : false;
 			// barChartData.datasets.forEach(function(dataset) {
 			// 	dataset.data = dataset.data.map(function() {
@@ -82,67 +275,66 @@ var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'A
 			// 	});
 
 			// });
-			var t=0;
-			var data_2016 = []
-			for(var i=0;i<DataArray.length;i++){
-				if(DataArray[i][0] == '2016'){
-					data_2016[t] = DataArray[i][3];
-					console.log(DataArray[i][3]);
-					t++;
-				}
-			}
-			barChartData.datasets[0].data = data_2016;
-			window.myBar.update();
-		});
+		// 	var t=0;
+		// 	var data_2016 = []
+		// 	for(var i=0;i<DataArray.length;i++){
+		// 		if(DataArray[i][0] == '2016'){
+		// 			data_2016[t] = DataArray[i][3];
+		// 			t++;
+		// 		}
+		// 	}
+		// 	barChartData.datasets[0].data = data_2016;
+		// 	window.myBar.update();
+		// });
 
-		var colorNames = Object.keys(window.chartColors);
-		document.getElementById('addDataset').addEventListener('click', function() {
-			var colorName = colorNames[barChartData.datasets.length % colorNames.length];
-			var dsColor = window.chartColors[colorName];
-			var newDataset = {
-				label: 'Dataset ' + (barChartData.datasets.length + 1),
-				backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-				borderColor: dsColor,
-				borderWidth: 1,
-				data: []
-			};
+		// var colorNames = Object.keys(window.chartColors);
+		// document.getElementById('addDataset').addEventListener('click', function() {
+		// 	var colorName = colorNames[barChartData.datasets.length % colorNames.length];
+		// 	var dsColor = window.chartColors[colorName];
+		// 	var newDataset = {
+		// 		label: 'Dataset ' + (barChartData.datasets.length + 1),
+		// 		backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+		// 		borderColor: dsColor,
+		// 		borderWidth: 1,
+		// 		data: []
+		// 	};
 
-			for (var index = 0; index < barChartData.labels.length; ++index) {
-				newDataset.data.push(randomScalingFactor());
-			}
+		// 	for (var index = 0; index < barChartData.labels.length; ++index) {
+		// 		newDataset.data.push(randomScalingFactor());
+		// 	}
 
-			barChartData.datasets.push(newDataset);
-			window.myBar.update();
-		});
+		// 	barChartData.datasets.push(newDataset);
+		// 	window.myBar.update();
+		// });
 
-		document.getElementById('addData').addEventListener('click', function() {
-			if (barChartData.datasets.length > 0) {
-				var month = MONTHS[barChartData.labels.length % MONTHS.length];
-				barChartData.labels.push(month);
+		// document.getElementById('addData').addEventListener('click', function() {
+		// 	if (barChartData.datasets.length > 0) {
+		// 		var month = MONTHS[barChartData.labels.length % MONTHS.length];
+		// 		barChartData.labels.push(month);
 
-				for (var index = 0; index < barChartData.datasets.length; ++index) {
-					// window.myBar.addData(randomScalingFactor(), index);
-					barChartData.datasets[index].data.push(randomScalingFactor());
-				}
+		// 		for (var index = 0; index < barChartData.datasets.length; ++index) {
+		// 			// window.myBar.addData(randomScalingFactor(), index);
+		// 			barChartData.datasets[index].data.push(randomScalingFactor());
+		// 		}
 
-				window.myBar.update();
-			}
-		});
+		// 		window.myBar.update();
+		// 	}
+		// });
 
-		document.getElementById('removeDataset').addEventListener('click', function() {
-			barChartData.datasets.pop();
-			window.myBar.update();
-		});
+		// document.getElementById('removeDataset').addEventListener('click', function() {
+		// 	barChartData.datasets.pop();
+		// 	window.myBar.update();
+		// });
 
-		document.getElementById('removeData').addEventListener('click', function() {
-			barChartData.labels.splice(-1, 1); // remove the label first
+		// document.getElementById('removeData').addEventListener('click', function() {
+		// 	barChartData.labels.splice(-1, 1); // remove the label first
 
-			barChartData.datasets.forEach(function(dataset) {
-				dataset.data.pop();
-			});
+		// 	barChartData.datasets.forEach(function(dataset) {
+		// 		dataset.data.pop();
+		// 	});
 
-			window.myBar.update();
-        });
+		// 	window.myBar.update();
+        // });
         
         function loadBarChart(){
             var ctx = document.getElementById('canvas').getContext('2d');
--- public/js_files/main.js
+++ public/js_files/main.js
@@ -6,4 +6,10 @@ window.onload = function() {
     // loadScatterChart();
 };
 
+//barchart filters
+// function myFunction() {
+//     var x = document.getElementById("mySelect").value;
+//  	var y = document.getElementById("mySelect1").value;
+// 	document.getElementById("demo").innerHTML = "You selected: " + x + y;
+// }
 
